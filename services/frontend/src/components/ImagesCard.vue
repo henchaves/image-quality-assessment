c8487a042ef0aedc8394540ed5046c710eea5d35
Henrique Chaves <henriquechavesmm@gmail.com>
2023-01-20 01:04:57 +0100

    Mock two images in frontend

---
+++ services/frontend/src/components/ImagesCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card mb-4 container">
+
+    <div class="row">
+      <!-- create card to wrap everything-->
+
+      <div class="col-sm">
+        <Image :imageId="image1Id" :imageUrl="image1Url" />
+      </div>
+      <div class="col-sm">
+        <Image :imageId="image2Id" :imageUrl="image2Url" />
+      </div>
+      <div class="col-sm d-flex flex-column justify-content-center">
+        <!-- align middle -->
+        <button class="btn btn-primary" @click="vote">Duplicated</button><br>
+        <button class="btn btn-primary" @click="vote">Not duplicated</button>
+
+      </div>
+      <!-- <div class="images row">
+      <Image :imageId="image1Id" :imageUrl="image1Url" />
+      <Image :imageId="image2Id" :imageUrl="image2Url" />
+    </div> -->
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Image from './Image.vue';
+
+export default {
+  name: 'ImagesCard',
+  components: {
+    Image,
+  },
+  props: {
+    image1Id: String,
+    image1Url: String,
+    image2Id: String,
+    image2Url: String,
+  }
+}
+</script>
--- services/frontend/src/views/HomeView.vue
+++ services/frontend/src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
+  <h1>Hello</h1>
+
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" />
+     -->
+    <ImagesCard :image1Id="images[0].id" :image1Url="images[0].url" :image2Id="images[1].id" :image2Url="images[1].url" />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import ImagesCard from '@/components/ImagesCard.vue'
+
+const images = [
+  {
+    id: '1',
+    url: 'https://i.travelapi.com/hotels/57000000/56980000/56971500/56971471/e97b6e21_z.jpg'
+  },
+  {
+    id: '2',
+    url: 'https://i.travelapi.com/hotels/57000000/56980000/56971500/56971471/36de3432_z.jpg'
+  }
+]
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ImagesCard
+  },
+  data() {
+    return {
+      images: images
+    }
   }
 }
 </script>
