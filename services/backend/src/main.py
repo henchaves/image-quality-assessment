69b96ccdecb76b7e1580c85c2f15e6a0e4a68ab3
M171239 <m171239@saneago.com.br>
2023-01-24 16:04:22 +0100

    Third version

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,Response
 from fastapi.middleware.cors import CORSMiddleware
 import src.models as bd
+import src.models.post_result as pr
 import pandas as pd
 import json as json
 
@@ -29,5 +30,11 @@ def pair_images():
     images = bd.dataset_images()
     score  = bd.get_score_duplicated(db,duplicateds['image_id'].iloc[0], duplicateds['image_id'].iloc[1],duplicateds['group_id'].iloc[1] )
     merged_group = pd.merge(left=images, right=duplicateds, left_on=['group_id','image_id'], right_on=['group_id','image_id'])
-    json_result = json.dumps({'score': score, 'images': merged_group.to_json(orient="index")}) 
-    return Response(content = json_result,media_type='application/json')
+    parsedobject=json.loads(merged_group.to_json(orient="index"))
+    compoundobject={'score':score,'images':parsedobject}
+    return Response(content = json.dumps(compoundobject,indent=4),media_type='application/json')
+
+@app.post("/human_result/")
+async def create_item(item: pr.human_result):
+    print("teste")
+    return item
